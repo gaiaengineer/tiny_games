055a31c93267fd997d6f78b2cee3720c38c87776
Olga Alexeeva <olga.alexee@proton.me>
2024-11-13 19:17:31 +0100

    Added scripts for fetch gameplay

---
+++ fetch/SpawnManagerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnIntervalMin = 1.0f;
+    private float spawnIntervalMax = 5.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float spawnRange = Random.Range(spawnIntervalMin, spawnIntervalMax);
+        Invoke("SpawnRandomBall", spawnRange);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[0].transform.rotation);
+
+        float spawnRange = Random.Range(spawnIntervalMin, spawnIntervalMax);
+        Invoke(nameof(SpawnRandomBall), spawnRange);
+    }
+
+}
